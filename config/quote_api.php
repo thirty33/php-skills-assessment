5ca64b0a7b957cda1e8713a109f86888d76beef8
joelSuarez <joel.suarez@linktic.co>
2024-04-02 21:34:29 -0400

    migrate configuration to config file

---
+++ config/quote_api.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'quotes_api_middlewares' => [
+		'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+    'throttle:default',
+    'can:user-favorites'
+	],
+	'quotes_rate_limit' => 30,
+	'quotes_api_prefix' => 'v1' 
+];
--- routes/web.php
+++ routes/web.php
@@ -30,19 +30,9 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+
     Route::get('/dashboard', [\App\Http\Controllers\PageController::class, 'dashboard'])
         ->name('dashboard');
-
-    Route::resource('quotes', \App\Http\Controllers\QuoteController::class)->middleware('can:user-favorites');
-
-    Route::post('quotes/add-favorite', [\App\Http\Controllers\QuoteController::class, 'favorite'])
-        ->name('quotes.favorite')->middleware('can:user-favorites');
-
-    Route::get('quotes/get-favorites/{user}', [\App\Http\Controllers\QuoteController::class, 'getFavoritesByUser'])
-        ->name('quotes.favorites')->middleware('can:user-favorites');
-
-    Route::delete('quotes/delete-favorite/{quote}', [\App\Http\Controllers\QuoteController::class, 'deleleFavorite'])
-        ->name('quotes.favorites.delete')->middleware('can:user-favorites');
 
     Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])
         ->name('users.index')
@@ -51,8 +41,24 @@ Route::middleware([
     Route::post('users/enable/{id}', [\App\Http\Controllers\UserController::class, 'enable'])
         ->name('users.enable')
         ->middleware('can:user-management');
-        
+
     Route::post('users/disable/{user}', [\App\Http\Controllers\UserController::class, 'disable'])
         ->name('users.disable')
         ->middleware('can:user-management');
+});
+
+Route::prefix(config('quote_api.quotes_api_prefix'))
+    ->middleware(config('quote_api.quotes_api_middlewares'))
+    ->group(function () {
+
+        Route::resource('quotes', \App\Http\Controllers\QuoteController::class)->middleware('can:user-favorites');
+
+        Route::post('quotes/favorites', [\App\Http\Controllers\QuoteController::class, 'favorite'])
+            ->name('quotes.favorite');
+
+        Route::get('quotes/favorites/{user}', [\App\Http\Controllers\QuoteController::class, 'getFavoritesByUser'])
+            ->name('quotes.favorites');
+
+        Route::delete('quotes/favorite/{quote}', [\App\Http\Controllers\QuoteController::class, 'deleleFavorite'])
+            ->name('quotes.favorites.delete');
 });
